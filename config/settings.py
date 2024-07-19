5d9dd88dcfeeb063c50e986f34c742626d637e1e
KOYAMA SHOYA <s1f102103189@iniad.org>
2024-07-19 19:38:24 +0900

    ChatGPT APIでテキスト変換

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,33 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+import environ
+
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+
+OPENAI_API_URL = config('OPENAI_API_URL')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a5$csl9qb9ys_*668#bdbx!%$4xuuuwzy=bjjaenjtpv(#a@2('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+]
 
 
 # Application definition
--- positive/views.py
+++ positive/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.urls import reverse
+from django.conf import settings
 from .forms import PostForm
 from .models import Post
 from .utils import is_positive  # ステップ 4で定義した関数をインポート
+
+from langchain.agents import Tool, initialize_agent, AgentType
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain_community.chat_models import ChatOpenAI
+
+OPENAI_API_KEY = settings.OPENAI_API_KEY
+OPENAI_API_BASE = settings.OPENAI_API_URL
 
 def post_create(request):
     posts = Post.objects.all()  # データベースからすべての投稿を取得
@@ -17,6 +25,14 @@ def post_create(request):
                 Post.objects.create(text=text)  # テキストをデータベースに保存
                 # 成功した場合、すべての投稿を含むページを再度表示
                 return render(request, 'positive/post_form.html', {'form': PostForm(), 'posts': posts})
+            else:
+                chat = ChatOpenAI(openai_api_key=OPENAI_API_KEY, openai_api_base=OPENAI_API_BASE, model_name='gpt-4o-mini', temperature=0)
+                content = f"「{text}」をポジティブな絵文字に変換してください。また、「{text}」は削除してください。"
+                messages = [
+                    HumanMessage(content=content),
+                    ]
+                result = chat(messages)
+                Post.objects.create(text=result.content)
     else:
         form = PostForm()
     # GETリクエストの場合、またはフォームが表示された直後
